f44d8e9d528885824a14ec8a0ad920cca88e6634
Don Pistole <junkmail@donaldpistole.com>
2017-12-31 03:54:28 -0700

    let there be light

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -14,38 +14,48 @@ class HomeLayout extends Component {
         return (
             <div id="home-layout" className={timeOfDay}>
                 <div className="nav">
-                    <div className="controls">
-                        { timeOfDay === timesOfDay.DAY && 
-                            <button onClick={() => setTimeOfDay(timesOfDay.NIGHT)}>
-                                <i className="fa fa-sun-o" />
-                            </button>
-                        }
-                        { timeOfDay === timesOfDay.NIGHT && 
-                            <button onClick={() => setTimeOfDay(timesOfDay.DAY)}>
-                                <i className="fa fa-moon-o" />
-                            </button>
-                        }
-                    </div>
-                    <div>
-                        Don Pistole
-                    </div>
-                    <div className="social-links">
-                        <a href="https://github.com/dpistole/" target="_new">
-                            <i className="fa fa-github" />
-                        </a>
-                        <a href="https://twitter.com/DonaldPistole" target="_new">
-                            <i className="fa fa-twitter" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/donald-pistole/" target="_new">
-                            <i className="fa fa-linkedin" />
-                        </a>
+                    <div className="container">         
+                        <div className="controls">
+                            { timeOfDay === timesOfDay.DAY && 
+                                <button onClick={() => setTimeOfDay(timesOfDay.NIGHT)}>
+                                    <i className="fa fa-sun-o" />
+                                </button>
+                            }
+                            { timeOfDay === timesOfDay.NIGHT && 
+                                <button onClick={() => setTimeOfDay(timesOfDay.DAY)}>
+                                    <i className="fa fa-moon-o" />
+                                </button>
+                            }
+                        </div>
+                        <div>
+                            Don Pistole
+                        </div>
+                        <div className="social-links">
+                            <a href="https://github.com/dpistole/" target="_new">
+                                <i className="fa fa-github" />
+                            </a>
+                            <a href="https://twitter.com/DonaldPistole" target="_new">
+                                <i className="fa fa-twitter" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/donald-pistole/" target="_new">
+                                <i className="fa fa-linkedin" />
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div className="sky">
+                    <div className="container">
+                        <div className="row">
+                            <div className="column">
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="top-of-ground">
+                    <div className="container"/>
                 </div>
                 <div className="front-of-ground">
+                    <div className="container"/>
                 </div>
             </div>
         );
