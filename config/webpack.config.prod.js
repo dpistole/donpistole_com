f0b9bc657b855574d76b09399d14797df147b019
Don Pistole <junkmail@donaldpistole.com>
2018-05-06 16:06:54 -0700

    add svg loader to webpack prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -217,6 +217,11 @@ module.exports = {
               loader: "sass-loader" // compiles Sass to CSS
             }]
           },
+          {
+            test: /\.svg$/,
+            exclude: /assets/,
+            loader: 'svg-inline-loader',
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
