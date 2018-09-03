07dd3321c5c6e6cbf6953550c85ddffec9bedb07
Don Pistole <junkmail@donaldpistole.com>
2018-09-03 02:20:57 -0700

    its better than nothing

--- src/layouts/AboutMe/index.js
+++ src/layouts/AboutMe/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class AboutMe extends Component {
   constructor(props){
@@ -11,6 +12,30 @@ class AboutMe extends Component {
     return (
       <div className="about-me-layout">
         <div className="container">
+
+          <div className="things-i-like">
+            <p className="header">Things I Like</p>
+            <ul>
+              <li>The Internet</li>
+              <li>My Motorcyle</li>
+              <li>Pizza</li>
+              <li>Poker</li>
+              <li>Podcasts</li>
+            </ul>
+          </div>
+        
+          <div className="things-i-know">
+            <p className="header">Things I Know</p>
+            <ul>
+              <li>
+                <Link title="Web Development" to="/">
+                  Web Development
+                </Link>
+              </li>
+              <li>Systems Administration</li>
+              <li>Networking</li>
+            </ul>
+          </div>
         </div>
       </div>
     )
